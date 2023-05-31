4d9bf74897bf2d364ec280ec3327255e80a6e5c6
22th Century Boy <55673958+matismurf@users.noreply.github.com>
2023-05-31 18:40:11 -0300

    Add files via upload

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    backgroundColor: '$gray100',
+    color: '$gray900',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  }
+})
